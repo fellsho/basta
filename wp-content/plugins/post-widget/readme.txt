bf6ff31ba7f25ed366ed660fc7d598f115ab9bb7
emin1 <fellsho@gmail.com>
2016-06-28 10:44:46 +0200

    Prvi commit i dodavanje aplikacije

---
+++ wp-content/plugins/post-widget/readme.txt
@@ -0,0 +1,39 @@
+=== Post Widget ===
+Contributors: wplimb
+Donate link: http://wplimb.com
+Tags: posts widget, latest posts, widget, latest posts widget, posts widget, recent post, post thumbnails
+Requires at least: 4.0
+Tested up to: 4.4.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Post Widget to display recent posts.
+
+== Description ==
+
+Post Widget is a very powerful widget plugin for WordPress which displays your most recent or latest posts with thumbnails. This is perfect for simple blogs & online magazines. Widgets come with highly-customizable control panels. Easy to use and support multi-widget even in the same sidebar.
+
+
+
+
+
+
+== Installation ==
+
+1. Download the plugin.
+2. Upload it to the plugins folder of your blog.
+3. Activate the plugin through the 'Plugins' menu in WordPress
+4. Now, goto the Widgets page of the Appearance section and configure the Post Widget.
+
+
+
+== Screenshots ==
+
+
+
+== Changelog ==
+
+= 1.0 =
+* First Release
+
+
---
+++ wp-content/themes/twentysixteen-child/template-parts/content.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The template part for displaying content
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+	
+
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+	</header><!-- .entry-header -->
+
+	<?php twentysixteen_excerpt(); ?>
+
+	<?php twentysixteen_post_thumbnail(); ?>
+
+	<div class="entry-content">
+		<?php
+			/* translators: %s: Name of current post */
+			the_content( sprintf(
+				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+				get_the_title()
+			) );
+
+
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php twentysixteen_entry_meta(); ?>
+		<?php
+			edit_post_link(
+				sprintf(
+					/* translators: %s: Name of current post */
+					__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+					get_the_title()
+				),
+				'<span class="edit-link">',
+				'</span>'
+			);
+		?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->

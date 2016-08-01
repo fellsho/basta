65b108fbf1674b429ff293874358b8d1d14935c8
Emin <fellsho@gmail.com>
2016-08-01 21:43:08 +0200

    imjena 23

--- wp-content/themes/twentysixteen-child/header.php
+++ wp-content/themes/twentysixteen-child/header.php
@@ -45,7 +45,7 @@
 
 				<?php if ( has_nav_menu( 'primary' ) || has_nav_menu( 'social' ) ) : ?>
 					<button id="menu-toggle" class="menu-toggle"><?php _e( 'Menu', 'twentysixteen' ); ?></button>
-
+</div><!-- .site-header-main -->
 					<div id="site-header-menu" class="site-header-menu">
 						<?php if ( has_nav_menu( 'primary' ) ) : ?>
 							<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Primary Menu', 'twentysixteen' ); ?>">
@@ -73,7 +73,7 @@
 						<?php endif; ?>
 					</div><!-- .site-header-menu -->
 				<?php endif; ?>
-			</div><!-- .site-header-main -->
+			
 
 			<?php if ( get_header_image() ) : ?>
 				<?php
